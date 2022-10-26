8a40740ac979f00293b2e6129f3c7cc2bc990bbe
yessm621 <yessm621@gmail.com>
2022-10-26 14:09:28 +0900

    DataSource 사용

---
+++ src/test/java/com/study/demo/connection/ConnectionTest.java
@@ -0,0 +1,35 @@
+package com.study.demo.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.study.demo.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSource() throws SQLException {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
